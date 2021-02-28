91cd24ae0b09eded82f2fc054d1715f8b28576a8
henriqueritter <henrique-ritter@hotmail.com>
2021-02-28 08:45:04 -0300

    Test user 2

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,11 +3,18 @@ import {createConnection,Connection, getConnectionOptions} from 'typeorm';
 export default async ():Promise<Connection>=>{
   //pega as configs do ORM Config
   const defaultOptions=await getConnectionOptions();
+  
+  if(!process.env.NODE_ENV){
+    process.env.NODE_ENV='qalquer';
+  }
+
   return createConnection(
     Object.assign(defaultOptions,{
-      database:process.env.NODE_ENV === 'test' 
-      ? './src/database/database.test.sqlite' 
-      : defaultOptions.database
+      database:
+        process.env.NODE_ENV === 'test' 
+        ? './src/database/database.test.sqlite' 
+        : defaultOptions.database
     })
+    
   );
 }
