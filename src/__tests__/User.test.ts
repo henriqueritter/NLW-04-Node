f943bf3e5fdb51330491609eabc2039f598f0011
henriqueritter <henrique-ritter@hotmail.com>
2021-02-28 08:30:58 -0300

    Test User

--- src/__tests__/User.test.ts
+++ src/__tests__/User.test.ts
@@ -1,10 +1,22 @@
 import request from 'supertest';
 import {app} from '../app';
 
+import createConnection from '../database';
+
 describe('Users', ()=>{
-  request(app).post("/user")
-  .send({
-    email: 'user@example.com',
-    name:'User Example'
+
+  //roda as migrations do Banco de test
+  beforeAll(async ()=>{
+    const connection= await createConnection();
+    await connection.runMigrations();
+  })
+
+  it("should be able to create a new user", async ()=>{
+    const response = await request(app).post("/users")
+    .send({
+      email: 'user@example.com',
+      name:'User Example'
+    });
+    expect(response.status).toBe(201);
   })
 })
